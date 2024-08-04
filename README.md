89df9265dd55d5799c33b6d5e8d87937543369b6
FatihYigit35 <fatih.yigit.35@gmail.com>
2024-08-04 23:34:13 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# shopping_notes
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/shopping_notes/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.shopping_notes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
