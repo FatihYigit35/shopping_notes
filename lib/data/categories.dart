14a594e68d4e2e09016571f2b4888bdd04b16b95
FatihYigit35 <fatih.yigit.35@gmail.com>
2024-08-05 00:21:04 +0300

    The application has started
    Co-Authored-By: izmirsoftware <168662977+izmirsoftware@users.noreply.github.com>

---
+++ lib/data/categories.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_notes/model/category.dart';
+
+const categories = {
+  Categories.vegetables: Category(
+    'Vegetables',
+    color: Color.fromARGB(255, 0, 255, 128),
+  ),
+  Categories.fruit: Category(
+    'Fruit',
+    color: Color.fromARGB(255, 145, 255, 0),
+  ),
+  Categories.meat: Category(
+    'Meat',
+    color: Color.fromARGB(255, 255, 102, 0),
+  ),
+  Categories.dairy: Category(
+    'Dairy',
+    color: Color.fromARGB(255, 0, 208, 255),
+  ),
+  Categories.carbs: Category(
+    'Carbs',
+    color: Color.fromARGB(255, 0, 60, 255),
+  ),
+  Categories.sweets: Category(
+    'Sweets',
+    color: Color.fromARGB(255, 255, 149, 0),
+  ),
+  Categories.spices: Category(
+    'Spices',
+    color: Color.fromARGB(255, 255, 187, 0),
+  ),
+  Categories.convenience: Category(
+    'Convenience',
+    color: Color.fromARGB(255, 191, 0, 255),
+  ),
+  Categories.hygiene: Category(
+    'Hygiene',
+    color: Color.fromARGB(255, 149, 0, 255),
+  ),
+  Categories.other: Category(
+    'Other',
+    color: Color.fromARGB(255, 0, 225, 255),
+  ),
+};
+
+enum Categories {
+  vegetables,
+  fruit,
+  meat,
+  dairy,
+  carbs,
+  sweets,
+  spices,
+  convenience,
+  hygiene,
+  other
+}
---
+++ lib/model/category.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Category {
+  const Category(this.title, {this.color = Colors.green});
+
+  final String title;
+  final Color color;
+}
