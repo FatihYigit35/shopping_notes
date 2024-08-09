4891114d6ba41e9135dd3383aed3ff9646b7c1e1
FatihYigit35 <fatih.yigit.35@gmail.com>
2024-08-10 00:50:13 +0300

    added navigation to page created for new items

--- lib/widget/shopping_notes.dart
+++ lib/widget/shopping_notes.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_notes/data/dummy_items.dart';
+import 'package:shopping_notes/widget/new_item.dart';
 import 'package:shopping_notes/widget/shopping_note_item.dart';
 
-class ShoppingNotes extends StatelessWidget {
+class ShoppingNotes extends StatefulWidget {
   const ShoppingNotes({super.key});
+
+  @override
+  State<ShoppingNotes> createState() => _ShoppingNotesState();
+}
+
+class _ShoppingNotesState extends State<ShoppingNotes> {
+  void _addItem() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const NewItem(),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,14 +26,20 @@ class ShoppingNotes extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Shopping Notes'),
+        actions: [
+          IconButton(
+            onPressed: _addItem,
+            icon: const Icon(
+              Icons.add,
+            ),
+          ),
+        ],
       ),
       body: ListView.builder(
         itemCount: list.length,
-        itemBuilder: (context, index) {
-          return ShoppingNoteItem(
-            item: list[index],
-          );
-        },
+        itemBuilder: (context, index) => ShoppingNoteItem(
+          item: list[index],
+        ),
       ),
     );
   }
